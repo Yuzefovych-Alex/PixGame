d0f928d92636e4ee3fd6dbe8df213a27d2eea305
Yuzefovych-Alex <yuzefovych.alex@gmail.com>
2025-02-26 13:31:49 +0100

    test-5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 //import Image from "next/image";
 import Header from "@/components/Layout/Header";
 //import Footer from "@/components/Layout/Footer";
-//import MainHome from "@/components/Pages/MainHome";
+import MainHome from "@/components/Pages/MainHome";
 import Registr from "@/components/Pages/Registr";
 
 export default function Home() {
     return (
         <>
             <Header/>
-            <Registr/>
+            <MainHome/>
         </>
     );
 }
