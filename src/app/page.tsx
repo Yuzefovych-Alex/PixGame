cc803465a8d71da2e409b9123248d6b76ab80705
Yuzefovych-Alex <yuzefovych.alex@gmail.com>
2025-02-28 21:53:16 +0100

    reboot

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Header from "@/components/Layout/Header";
 //import Footer from "@/components/Layout/Footer";
 import MainHome from "@/components/Pages/MainHome";
 //import Registr from "@/components/Pages/Registr";
+import "@/app/global.scss";
 
 export default function Home() {
     return (
